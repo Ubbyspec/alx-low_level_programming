0df09923fb60c62d0d76b13ae4dabf09f5da7130
Ubbyspec <Ubaydatullah97@gmail.com>
2023-04-22 11:17:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- *  puzzle, followed by a new line."
+ *  puzzle, followed by a newline."
  *  return 0.
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzle);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
